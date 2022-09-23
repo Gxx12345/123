a7df6557210d6b24742c3cb8e916ba35d79baac2
郭云杰 <yjiiie@126.com>
2022-09-23 14:56:17 +0800

    feat:reggie.java

---
+++ reggie_take_out/src/main/java/com/itheima/reggie/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.itheima.reggie.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+
+//配置其不在static包下的资源
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    /**
+     * 设置静态资源映射
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+
+}
