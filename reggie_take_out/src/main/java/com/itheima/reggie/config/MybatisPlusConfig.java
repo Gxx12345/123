009856fc9422321ec49851178b2fff50493a87c4
mayao <mayao@itcast.cn>
2022-09-24 17:20:01 +0800

    添加和分页功能配置类

--- reggie_take_out/src/main/java/com/itheima/reggie/config/MybatisPlusConfig.java
+++ reggie_take_out/src/main/java/com/itheima/reggie/config/MybatisPlusConfig.java
@@ -1,10 +1,22 @@
 package com.itheima.reggie.config;
 
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Configuration;
 /**
  * MybatisPlus配置类
  *
  * @author my
  * @since 2022/9/24 17:09
  */
+@Configuration
 public class MybatisPlusConfig {
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        //mp的拦截器
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        //要在拦截器中添加分页插件
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+
+        return interceptor;
+    }
 }
