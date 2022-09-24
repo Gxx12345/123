4b2083db2f4bff064195aa7bc487b45980f48b67
xurui <673524512@qq.com>
2022-09-24 10:58:57 +0800

    reggit_take_out

--- reggit_take_out/src/main/java/com/itheima/reggie/ReggieApplication.java
+++ reggit_take_out/src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -5,6 +5,7 @@ import com.itheima.reggie.entity.Employee;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class,args);
