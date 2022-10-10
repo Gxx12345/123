8c23f11d822b342ebede6b25341d1306db3f40e4
cyberengr <cyberengr@163.com>
2022-10-10 11:06:18 +0800

    配置yml分页

--- reggie_cyberengr/src/main/java/com/alibaba/reggie/ReggieApplication.java
+++ reggie_cyberengr/src/main/java/com/alibaba/reggie/ReggieApplication.java
@@ -2,7 +2,9 @@ package com.alibaba.reggie;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
